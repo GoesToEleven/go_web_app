e08e98ee8801ab146f4935e4877b541601cca6d9
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2015-05-04 13:36:36 -0700

    set content type not quite working yet

--- src/main/main.go
+++ src/main/main.go
@@ -3,26 +3,36 @@ package main
 import (
     "net/http"
     "io/ioutil"
-    "log"
+    "strings"
 )
-
-// this is number 6
 
 type MyHandler struct {
 }
 
 func (this *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     path := r.URL.Path[1:]
-//    path := "templates" + r.URL.Path
-    log.Println("PATH: ", path)
-
     data, err := ioutil.ReadFile(string(path))
 
     if err == nil {
+        var contentType string
+
+        if strings.HasSuffix(path, ".css") {
+            contentType = "text/css"
+        } else if strings.HasSuffix(path, ".html") {
+            contentType = "text/html"
+        } else if strings.HasSuffix(path, ".js") {
+            contentType = "application/javascript"
+        } else if strings.HasSuffix(path, ".png") {
+            contentType = "image/png"
+        } else {
+            contentType = "text/plain"
+        }
+
+        w.Header().Add("Content Type", contentType)
         w.Write(data)
     } else {
         w.WriteHeader(404)
-        w.Write([]byte("404 My Friend - " + http.StatusText(404)))
+        w.Write([]byte("404 Mi amigo - " + http.StatusText(404)))
     }
 }
 
@@ -32,41 +42,5 @@ func main() {
 }
 
 /*
-structuring go apps
 
-*****************
-SUPER IMPORTANT
-*****************
-
-ROOT is determined by ...
-WHERE YOU ARE IN YOUR DIRECTORY WHEN YOU RUN "GO RUN"
-wherever you are in your dir when you run "go run" - that's your root
-
-for example
-if this is your file structure
-go_web_app
--- bin
--- pkg
--- src
----- main
------- main.go
--- templates
----- home.html
-
-if you were in "go_web_app / src / main"
-and you ran "go run main.go"
-then the root of your files would be "go_web_app / src / main"
-and you would not be able to access "templates" nor "templates/home.html"
-
-if you were in "go_web_app"
-and you ran "go run main.go"
-then the root of your files would be "go_web_app"
-and you would be able to access "templates" and "templates/home.html"
-
-this is a good article - though advanced (so don't freak out if you're just getting started)
-https://medium.com/@benbjohnson/structuring-applications-in-go-3b04be4ff091
-
-this can also be useful in debugging:
--- import "os"
----- log.Println("ENV: ", os.Environ())
 */
