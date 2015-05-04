2c0d445bfc2e64f4bf1a5cc02e1011cb84c83e8c
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2015-05-04 14:57:47 -0700

    adds fileServer

--- src/main/main.go
+++ src/main/main.go
@@ -2,54 +2,9 @@ package main
 
 import (
     "net/http"
-    "strings"
-    "log"
-    "os"
-    "bufio"
 )
 
-type MyHandler struct {
-}
-
-func (this *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    path := r.URL.Path[1:]
-    log.Println(path)
-    f, err := os.Open(path)
-
-    if err == nil {
-        bufferedReader := bufio.NewReader(f)
-
-        var contentType string
-
-        if strings.HasSuffix(path, ".css") {
-            contentType = "text/css"
-        } else if strings.HasSuffix(path, ".html") {
-            contentType = "text/html"
-        } else if strings.HasSuffix(path, ".js") {
-            contentType = "application/javascript"
-        } else if strings.HasSuffix(path, ".png") {
-            contentType = "image/png"
-        } else if strings.HasSuffix(path, ".svg") {
-            contentType = "image/svg+xml"
-        } else if strings.HasSuffix(path, ".mp4") {
-            contentType = "video/mp4"
-        } else {
-            contentType = "text/plain"
-        }
-
-        w.Header().Add("Content Type", contentType)
-        bufferedReader.WriteTo(w)
-    } else {
-        w.WriteHeader(404)
-        w.Write([]byte("404 Mi amigo - " + http.StatusText(404)))
-    }
-}
 
 func main() {
-    http.Handle("/", new(MyHandler))
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":8080", http.FileServer(http.Dir("")))
 }
-
-/*
-
-*/
